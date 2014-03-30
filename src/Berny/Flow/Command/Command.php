1442f92ece264ddfb62b110808ff580af0aae1ac
Berny Cantos <be@rny.cc>
2014-03-30 11:20:10 +0200

    Abstract Command for Application

---
+++ src/Berny/Flow/Command/Command.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * This file is part of Berny\Flow
+ *
+ * (c) Berny Cantos <be@rny.cc>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Berny\Flow\Command;
+
+use Symfony\Component\Console\Command\Command as BaseCommand;
+
+abstract class Command extends BaseCommand
+{
+}

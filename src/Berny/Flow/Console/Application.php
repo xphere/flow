5cb9a5c6a57325825b5904eea47091a62d1ae402
Berny Cantos <be@rny.cc>
2014-03-30 01:36:45 +0100

    add Application

---
+++ src/Berny/Flow/Console/Application.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * This file is part of Berny\Flow
+ *
+ * (c) Berny Cantos <be@rny.cc>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Berny\Flow\Console;
+
+use Symfony\Component\Console;
+
+/**
+ * Access point to the commands
+ */
+class Application extends Console\Application
+{
+}
